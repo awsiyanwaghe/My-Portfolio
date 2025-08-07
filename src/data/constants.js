c3458694a0ae0b4d728134e5bb4d504045f24a0b
Awsiyan waghe <awsiyanw@gmail.com>
2025-08-07 12:41:42 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -19,7 +19,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/awsiyanwaghe",
   resume:
-    "https://drive.google.com/file/d/1ZGBPqYkcZxhqmf7Ix7UpEWxvwQSrvpwl/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1_DQBoaMZlwNcmA2anHym2uHEy0hDj6la/view?usp=drivesdk",
   twitter: "https://x.com/awsiyan_waghe",
   insta: "https://www.instagram.com/awsiyan_waghe/",
   leetcode: "https://leetcode.com/u/jcBd2SRsgw/",
